02b9756297fe738f1e18881ab673489cd7ff221e
Alexander Bowers <7319abowers@gmail.com>
2020-09-25 04:30:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,10 +2,19 @@
 
 
 def position_taken? (board, index)
+<<<<<<< HEAD
   if board[index] == "X" || board[index] == "O"
     return true
   elsif board[index] == " " || "" || "nil"
     return false
+=======
+  if board[index] == " "
+    return false
+  elsif board[index] == "" || "nil"
+    return false
+  elsif board[index] == "X" || "O"
+    return true
+>>>>>>> 14c765fb8e0c0df14b486e1da106d0bfa579cf15
   else
     return true
   end
